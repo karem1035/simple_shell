2db854761a1788e8a42b962e4136a6f7816c78a5
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-14 21:09:55 +0300

    create shell header

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+extern char **environ;
+
+
+void _puts(char *str);
+int _putchar(char c);
+#endif
