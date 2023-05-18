9f26d8e530ad276cd9b01cb47489f7b5b4cfd8a9
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-19 01:13:33 +0300

    adding functions

--- shell.h
+++ shell.h
@@ -9,12 +9,18 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-struct stat st;
 
 
 extern char **environ;
-void execute(char *argv, char *argvv[]);
-
+void execute(char *argvv[]);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+int _which(char *argvv[]);
+unsigned int var_len(char *environ_i);
+unsigned int val_len(char *environ_i);
+int _strcmp(char *s1, char *s2);
+char *_getenv(char *name);
 
 #define MAX_TOKENS 20
 #endif
