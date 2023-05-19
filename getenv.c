d1dc52a738cf32726eb1dbe0585ab10e48c2954d
eyadfattah23 <eyadabdelfattah4@gmail.com>
2023-05-19 23:59:14 +0300

    adding strdup here

--- getenv.c
+++ getenv.c
@@ -1,4 +1,36 @@
 #include "shell.h"
+
+/**
+ * _strdup - returns a pointer to a copy of the string
+ * @str: the string to copy
+ * Return: a pointer to a copy of the string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	p = malloc(sizeof(char) * i);
+	i = 0;
+	if (p == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
 /**
  * var_len - get the var length.
  * @environ_i: the var name.
