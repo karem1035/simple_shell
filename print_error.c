86b1e572ff4b857c3de8fcf93062ad44eb5d35f9
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-05-23 06:17:09 +0300

    Print error updated int_to_str function retrieved.

--- print_error.c
+++ print_error.c
@@ -6,11 +6,11 @@
  * @cname: the command name.
  * Return: void.
  */
-void print_error(char *fname, unsigned int pnum, char *cname)
+void print_error(char *fname, unsigned int process_number, char *cname)
 {
 	char *number = NULL;
 
-	number = int_to_str(pnum, number);
+	number = int_to_str(process_number, number);
 
 	_puts2(fname);
 	_puts2(": ");
@@ -19,5 +19,33 @@ void print_error(char *fname, unsigned int pnum, char *cname)
 	_puts2(cname);
 	_puts2(": ");
 	_puts2("not found\n");
-	free(number);
+	if (!isatty(0))
+		exit(127);
+}
+/**
+ * int_to_str - converts int to char;
+ * @a: the int.
+ * @str: the str.
+ * Return: the char.
+ */
+char *int_to_str(unsigned int a, char *str)
+{
+	int digit = 0, n = a, i;
+
+	while (n != 0)
+	{
+		n /= 10;
+		digit++;
+	}
+
+	str = malloc(sizeof(char) * (digit + 1));
+	if (str == NULL)
+		return (NULL);
+	for (i = digit - 1; i >= 0; i--)
+	{
+		str[i] = (a % 10) + '0';
+		a /= 10;
+	}
+	str[digit] = '\0';
+	return (str);
 }
